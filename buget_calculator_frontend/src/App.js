f1396c109740fab6861d72d77fecc98dd1175a7a
Your Name <jhonlloydborlagdan8@gmail.com>
2024-07-19 22:37:23 +0800

    Modified App.js

--- buget_calculator_frontend/src/App.js
+++ buget_calculator_frontend/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
+
+function Home(){
+  return(
+    <div className="App">
+        <h1> Budget Calculator</h1>
+        {/* Select drop down here */}
+    </div>
+  )
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
