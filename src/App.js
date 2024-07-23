2c344faf76d65e689c8876cacd5d1b223acd6fd5
Your Name <jhonlloydborlagdan8@gmail.com>
2024-07-23 18:45:44 +0800

    For testing purpose only 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useEffect , useState} from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
 function Home() {
   const [selectedValue, setSelectedValue] = useState('');
   const [options, setOptions] = useState([]);
-  // const [setOptions] = useState([]);
-  // const [income, setIncome] = useState('');
-  // const [budget, setBudget] = useState({});
-  // const [selectedRule, setSelectedRule] = useState(null);
+  const [income, setIncome] = useState('');
+  const [budget, setBudget] = useState({});
+  const [selectedRule, setSelectedRule] = useState(null);
 
   useEffect(() => {
-
     axios.get('https://lloyd44560.pythonanywhere.com/api/rules/')
       .then(response => {
         setOptions(response.data);
@@ -27,46 +25,41 @@ function Home() {
     setSelectedValue(event.target.value);
   };
 
-  // const handleIncomeChange = (event) => {
-  //   setIncome(event.target.value);
-  // };
+  const handleIncomeChange = (event) => {
+    setIncome(event.target.value);
+  };
 
-  // const calculateBudget = () => {
-  //   const rule = options.find(option => option.id === parseInt(selectedValue));
-  //   setSelectedRule(rule);
-  //   if (rule && income) {
-  //     const budget = {
-  //       category1: (income * rule.percentage1) / 100,
-  //       category2: (income * rule.percentage2) / 100,
-  //       category3: rule.percentage3 ? (income * rule.percentage3) / 100 : 0
-  //     };
-  //     setBudget(budget);
-  //   } else {
-  //     setBudget({});
-  //   }
-  // };
+  const calculateBudget = useCallback(() => {
+    const rule = options.find(option => option.id === parseInt(selectedValue));
+    setSelectedRule(rule);
+    if (rule && income) {
+      const budget = {
+        category1: (income * rule.percentage1) / 100,
+        category2: (income * rule.percentage2) / 100,
+        category3: rule.percentage3 ? (income * rule.percentage3) / 100 : 0
+      };
+      setBudget(budget);
+    } else {
+      setBudget({});
+    }
+  }, [selectedValue, income, options]);
 
-  // useEffect(() => {
-  //   calculateBudget();
-  // }, [selectedValue, income]);
+  useEffect(() => {
+    calculateBudget();
+  }, [selectedValue, income, calculateBudget]);
 
   return (
     <div className="App">
       <h1>Budget Calculator</h1>
-{
-
-    <label>
-      Choose a rule:
-      <select value={selectedValue} onChange={handleSelectChange}>
-        <option value="">Select...</option>
-        {options.map(option => (
-          <option key={option.id} value={option.id}>{option.name}</option>
-        ))}
-      </select>
-    </label>
-
-/* 
-     
+      <label>
+        Choose a rule:
+        <select value={selectedValue} onChange={handleSelectChange}>
+          <option value="">Select...</option>
+          {options.map(option => (
+            <option key={option.id} value={option.id}>{option.name}</option>
+          ))}
+        </select>
+      </label>
       <br />
       <label>
         Enter your monthly income:
@@ -82,7 +75,7 @@ function Home() {
             <p>Category 3: ${budget.category3 ? budget.category3.toFixed(2) : 'N/A'}</p>
           )}
         </div>
-      )} */}
+      )}
     </div>
   );
 }
